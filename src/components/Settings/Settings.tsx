d4b1922d7fb155333923d5379df5f0eb2e806f25
sergij14 <sergi.jajanidze@gmail.com>
2022-06-11 20:51:17 +0400

    extracted settings logic to the custom hook

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,28 +1,10 @@
-import { useForm } from "react-hook-form";
-import { NavigationMenuData } from "../Navigation/Navigation.types";
 import MenuFields from "./Fields/MenuFields";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { menuSchema } from "./Settings.schema";
-import useNavigation from "../Navigation/useNavigation";
-import { useNavigate } from "react-router-dom";
+import useSettings from "./useSettings";
 
 export default function Settings() {
-  const { typedStorage } = useNavigation();
-  const menus = typedStorage.getItem('menus');
-
-  const { control, register, handleSubmit, formState } =
-    useForm<NavigationMenuData>({
-      mode: "onChange",
-      defaultValues: menus ? { menus } : {},
-      resolver: yupResolver(menuSchema),
-    });
-
-  const navigate = useNavigate();
-  const onSubmit = (data: NavigationMenuData) => {
-    typedStorage.setItem("menus", data.menus);
-    navigate("/");
-  };
-
+  const { control, register, handleSubmit, formState, onSubmit } =
+    useSettings();
+    
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
---
+++ src/components/Settings/useSettings.ts
@@ -0,0 +1,32 @@
+import { useForm } from "react-hook-form";
+import { NavigationMenuData } from "../Navigation/Navigation.types";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { menuSchema } from "./Settings.schema";
+import useNavigation from "../Navigation/useNavigation";
+import { useNavigate } from "react-router-dom";
+
+export default function useSettings() {
+  const { typedStorage } = useNavigation();
+  const menus = typedStorage.getItem("menus");
+
+  const { control, register, handleSubmit, formState } =
+    useForm<NavigationMenuData>({
+      mode: "onChange",
+      defaultValues: menus ? { menus } : {},
+      resolver: yupResolver(menuSchema),
+    });
+
+  const navigate = useNavigate();
+  const onSubmit = (data: NavigationMenuData) => {
+    typedStorage.setItem("menus", data.menus);
+    navigate("/");
+  };
+
+  return {
+    control,
+    register,
+    handleSubmit,
+    formState,
+    onSubmit,
+  };
+}
